f57e5dd4e6b4f56cc7b7bd93ce37aa03fcf5848a
Fever SEO <seo.tools@feverup.com>
2025-04-16 12:08:12 +0200

    Update __init__.py
    Fixing the Import Hierarchy

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,11 +6,8 @@ This package contains various utility functions, file handlers, constants,
 and helper methods used throughout the application.
 """
 
-# Import utility functions
-from utils.file_handlers import process_uploaded_file, export_results
-from utils.constants import INTENT_COLORS, DEFAULT_NUM_CLUSTERS, APP_NAME, APP_DESCRIPTION
-from utils.helpers import generate_sample_csv
-from utils.state import initialize_session_state, update_session_state
-
 # Define utils version
 __version__ = '1.0.0'
+
+# Do not import here to avoid circular imports
+# Import statements should be in individual files instead
