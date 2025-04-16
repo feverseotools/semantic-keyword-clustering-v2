2ecf1825849dbef26012376128ef9fbb62fda1f4
Fever SEO <seo.tools@feverup.com>
2025-04-16 12:07:47 +0200

    Update __init__.py
    Fixing the Import Hierarchy

--- ui/__init__.py
+++ ui/__init__.py
@@ -6,11 +6,8 @@ This package contains all the UI-related functionality, including components,
 page layouts, visualizations, and styling for the Streamlit-based application.
 """
 
-# Import main UI functions
-from ui.styles import load_css
-from ui.components import display_cluster_card, show_filter_controls, show_metrics_summary
-from ui.pages import show_welcome_page, show_results_dashboard
-from ui.visualizations import show_intent_distribution, show_cluster_sizes
-
 # Define UI version
 __version__ = '1.0.0'
+
+# Do not import here to avoid circular imports
+# Import statements should be in individual files instead
